f005aa5109e645e6955c525cf55b16c2f582561e
ntostis <46918261+ntostis@users.noreply.github.com>
2019-02-15 13:20:05 +0200

    1t File

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+
+	public static void main(String[] arg) {
+	int age=10;
+	System.out.println("Hello, World");
+	System.out.println("mi age is " + age);
+	// test
+ }
+
+}
